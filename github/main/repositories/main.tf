e5408fe93ec2c9df489259d874b4b60271788f3b
Mateusz Umiński <17390661+mateusz-uminski@users.noreply.github.com>
2023-07-08 17:04:07 +0200

    feat: add github service to manage my repositories

---
+++ github/main/repositories/main.tf
@@ -0,0 +1,71 @@
+locals {
+  repositories = {
+    "terragrunt-services" = {
+      protected_branch = "main"
+      visibility       = "public"
+      license          = "mit"
+      required_status_checks = [
+        "terragrunt-hclfmt / lint",
+        "terraform-fmt / lint",
+      ]
+    },
+    "terraform-service-modules" = {
+      protected_branch = "main"
+      visibility       = "public"
+      license          = "mit"
+      required_status_checks = [
+        "terraform-fmt / lint",
+      ]
+    },
+    "terragrunt-aws-infrastructures" = {
+      protected_branch = "main"
+      visibility       = "public"
+      license          = "mit"
+      required_status_checks = [
+        "terragrunt-hclfmt / lint",
+        "terraform-fmt / lint",
+      ]
+    },
+    "terraform-aws-modules" = {
+      protected_branch = "main"
+      visibility       = "public"
+      license          = "mit"
+      required_status_checks = [
+        "terraform-fmt / lint",
+      ]
+    },
+    "dotfiles" = {
+      protected_branch       = "main"
+      visibility             = "public"
+      license                = "mit"
+      required_status_checks = []
+    },
+    "github-actions-shared-workflows" = {
+      protected_branch       = "main"
+      visibility             = "public"
+      license                = "mit"
+      required_status_checks = []
+    },
+    "pre-commit-hooks" = {
+      protected_branch       = "main"
+      visibility             = "public"
+      license                = "mit"
+      required_status_checks = []
+    },
+  }
+}
+
+
+module "repository" {
+  source = "git::https://github.com/mateusz-uminski/terraform-service-modules//github-repository?ref=github-repository/v0.1.0"
+
+  for_each = local.repositories
+
+  name = each.key
+
+  visibility = each.value.visibility
+  license    = each.value.license
+
+  protected_branch       = each.value.protected_branch
+  required_status_checks = each.value.required_status_checks
+}
